53a1f5e8bc5f3e420be869285469aa95d1aba76c
Michelle Brytowski <mn.brytowski@gmail.com>
2019-01-25 14:05:07 -0700

    more changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,13 +7,41 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         //who is the best match, var best match as an object (name, photo, difference) - starts empty
+        var bestMatch = {
+            name: '',
+            photo: '',
+            friendDifference: Infinity
+        };
         //set var to work with data
         var newFriend = req.body;
         var newFriendScores = newFriend.scores;
+        var scoreDifference = 0;
         //loop through friends and loop through scores (multi loops)
-        var difference = 0;
+        for (var i = 0; i < friendsData.length; i++) {
+            var currentFriend = friendsData[i];
+            let currentFriendScores = friends[i].scores
+            // bestMatch.push(currentFriend);
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+                let newFriendTotal = newFriendScores.reduce(reducer);
+                console.log("New Friend Total: " + newFriendTotal);
+
+                let currentFriendTotal = currentFriendScores.reduce(reducer);
+                console.log("Current Friend Total: " + currentFriendTotal);
+                
+                scoreDifference = Math.abs(newFriendTotal - currentFriendTotal);
+                if (scoreDifference < bestMatch.friendDifference) {
+                    bestMatch.push(currentFriend);
+                } else {
+                    return bestMatch;
+                }
+            }
+        }
+        console.log("Best Match: ", bestMatch);
+
         //define var outside loop = difference in scores - change value of difference var
-        var currentFriend = friends[i];
+
         //inner loop checks between current friend scores and new friend scores
         //
         friendsData.push(req.body);
